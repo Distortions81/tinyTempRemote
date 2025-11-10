abf91f04f0911384c77e94c0d671b9085c5d2f5e
Distortions81 <carlotto81@gmail.com>
2025-11-10 02:28:19 -0700

    faster i2c

--- main.go
+++ main.go
@@ -7,27 +7,142 @@ import (
 	"time"
 
 	"tinygo.org/x/drivers"
-	"tinygo.org/x/drivers/i2csoft"
+	"tinygo.org/x/drivers/mcp9808"
 	"tinygo.org/x/drivers/ssd1306"
 	"tinygo.org/x/tinyfont"
 	"tinygo.org/x/tinyfont/proggy"
 )
 
+// ---- Bit-banged I2C ----
+type SoftI2C struct {
+	SDA, SCL machine.Pin
+	delay    time.Duration
+}
+
+func (i2c *SoftI2C) Configure(freqHz int) {
+	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinOutputOpenDrain})
+	i2c.SCL.Configure(machine.PinConfig{Mode: machine.PinOutputOpenDrain})
+	i2c.SDA.High()
+	i2c.SCL.High()
+	i2c.delay = time.Second / time.Duration(freqHz*2)
+}
+
+func (i2c *SoftI2C) tick() { time.Sleep(i2c.delay) }
+
+func (i2c *SoftI2C) start() {
+	i2c.SDA.High()
+	i2c.SCL.High()
+	i2c.tick()
+	i2c.SDA.Low()
+	i2c.tick()
+	i2c.SCL.Low()
+}
+
+func (i2c *SoftI2C) stop() {
+	i2c.SDA.Low()
+	i2c.SCL.High()
+	i2c.tick()
+	i2c.SDA.High()
+	i2c.tick()
+}
+
+func (i2c *SoftI2C) writeByte(b byte) bool {
+	for i := 0; i < 8; i++ {
+		if b&0x80 != 0 {
+			i2c.SDA.High()
+		} else {
+			i2c.SDA.Low()
+		}
+		b <<= 1
+		i2c.tick()
+		i2c.SCL.High()
+		i2c.tick()
+		i2c.SCL.Low()
+	}
+	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinInput})
+	i2c.tick()
+	i2c.SCL.High()
+	ack := !i2c.SDA.Get()
+	i2c.SCL.Low()
+	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinOutputOpenDrain})
+	return ack
+}
+
+func (i2c *SoftI2C) readByte(ack bool) byte {
+	var b byte
+	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinInput})
+	for i := 0; i < 8; i++ {
+		i2c.SCL.High()
+		i2c.tick()
+		b = (b << 1)
+		if i2c.SDA.Get() {
+			b |= 1
+		}
+		i2c.SCL.Low()
+		i2c.tick()
+	}
+	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinOutputOpenDrain})
+	if ack {
+		i2c.SDA.Low()
+	} else {
+		i2c.SDA.High()
+	}
+	i2c.SCL.High()
+	i2c.tick()
+	i2c.SCL.Low()
+	i2c.SDA.High()
+	return b
+}
+
+// adapters SoftI2C to drivers.I2C
+type softI2CBus struct {
+	bus *SoftI2C
+}
+
+func (s *softI2CBus) Tx(addr uint16, w, r []byte) error {
+	if len(w) > 0 {
+		s.bus.start()
+		if !s.bus.writeByte(byte(addr<<1) | 0) {
+			s.bus.stop()
+			return fmt.Errorf("i2c addr 0x%02X NACK on write", addr)
+		}
+		for _, b := range w {
+			if !s.bus.writeByte(b) {
+				s.bus.stop()
+				return fmt.Errorf("i2c write NACK")
+			}
+		}
+		if len(r) == 0 {
+			s.bus.stop()
+		}
+	}
+	if len(r) > 0 {
+		s.bus.start()
+		if !s.bus.writeByte(byte(addr<<1) | 1) {
+			s.bus.stop()
+			return fmt.Errorf("i2c addr 0x%02X NACK on read", addr)
+		}
+		for i := range r {
+			r[i] = s.bus.readByte(i < len(r)-1)
+		}
+		s.bus.stop()
+	}
+	return nil
+}
+
 // ---- MCP9808 ----
-const MCP9808_ADDR = 0x18
+const mcp9808Addr = 0x18
 
-func readTempC(bus drivers.I2C) (float32, bool) {
-	data := []byte{0, 0}
-	if err := bus.Tx(MCP9808_ADDR, []byte{0x05}, data); err != nil {
-		return 0, false
+func newSensor(bus drivers.I2C) (*mcp9808.Device, error) {
+	sensor := mcp9808.New(bus)
+	sensor.Address = mcp9808Addr
+	if !sensor.Connected() {
+		return nil, fmt.Errorf("mcp9808 not detected")
 	}
-
-	raw := uint16(data[0])<<8 | uint16(data[1])
-	temp := float32(raw&0x0FFF) * 0.0625
-	if (raw & 0x1000) != 0 {
-		temp -= 256
+	if err := sensor.SetResolution(mcp9808.Maximum); err != nil {
+		return nil, err
 	}
-	return temp, true
+	return &sensor, nil
 }
 
 func blinkOnce(pin machine.Pin, duration time.Duration) {
@@ -38,8 +153,14 @@ func blinkOnce(pin machine.Pin, duration time.Duration) {
 
 // ---- Main ----
 func main() {
-	i2c := i2csoft.New(machine.D19, machine.D18)
-	i2c.Configure(i2csoft.I2CConfig{Frequency: 100000, SCL: machine.D19, SDA: machine.D18})
+	bitBang := &SoftI2C{SDA: machine.D18, SCL: machine.D19}
+	bitBang.Configure(100000)
+	i2c := &softI2CBus{bus: bitBang}
+
+	sensor, err := newSensor(i2c)
+	if err != nil {
+		panic(err)
+	}
 
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
@@ -52,9 +173,9 @@ func main() {
 	white := color.RGBA{255, 255, 255, 255}
 
 	for {
-		temp, ok := readTempC(i2c)
+		temp, tempErr := sensor.ReadTemperature()
 		display.ClearBuffer()
-		if ok {
+		if tempErr == nil {
 			blinkOnce(led, 100*time.Millisecond)
 			tinyfont.WriteLine(display, &proggy.TinySZ8pt7b, 0, 16,
 				fmt.Sprintf("Temp: %.2f C", temp), white)
