99f6e8d98b61da15cd44d7a2db2563abb3a0adbc
Distortions81 <carlotto81@gmail.com>
2025-11-10 02:15:30 -0700

    working

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tinygo build -target=teensy36 -o firmware.hex main.go
+tinygo build -target=teensy36 -o firmware.hex main.go && teensy_loader_cli -mmcu=TEENSY36 -w firmware.hex
--- main.go
+++ main.go
@@ -6,122 +6,21 @@ import (
 	"machine"
 	"time"
 
+	"tinygo.org/x/drivers"
+	"tinygo.org/x/drivers/i2csoft"
 	"tinygo.org/x/drivers/ssd1306"
 	"tinygo.org/x/tinyfont"
 	"tinygo.org/x/tinyfont/proggy"
 )
 
-// ---- Bit-banged I2C ----
-type SoftI2C struct {
-	SDA, SCL machine.Pin
-	delay    time.Duration
-}
-
-func (i2c *SoftI2C) Configure(freqHz int) {
-	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	i2c.SCL.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	i2c.SDA.High()
-	i2c.SCL.High()
-	i2c.delay = time.Second / time.Duration(freqHz*2)
-}
-
-func (i2c *SoftI2C) tick() { time.Sleep(i2c.delay) }
-
-func (i2c *SoftI2C) start() {
-	i2c.SDA.High()
-	i2c.SCL.High()
-	i2c.tick()
-	i2c.SDA.Low()
-	i2c.tick()
-	i2c.SCL.Low()
-}
-
-func (i2c *SoftI2C) stop() {
-	i2c.SDA.Low()
-	i2c.SCL.High()
-	i2c.tick()
-	i2c.SDA.High()
-	i2c.tick()
-}
-
-func (i2c *SoftI2C) writeByte(b byte) bool {
-	for i := 0; i < 8; i++ {
-		if b&0x80 != 0 {
-			i2c.SDA.High()
-		} else {
-			i2c.SDA.Low()
-		}
-		b <<= 1
-		i2c.tick()
-		i2c.SCL.High()
-		i2c.tick()
-		i2c.SCL.Low()
-	}
-	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinInput})
-	i2c.tick()
-	i2c.SCL.High()
-	ack := !i2c.SDA.Get()
-	i2c.SCL.Low()
-	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	return ack
-}
-
-func (i2c *SoftI2C) readByte(ack bool) byte {
-	var b byte
-	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinInput})
-	for i := 0; i < 8; i++ {
-		i2c.SCL.High()
-		i2c.tick()
-		b = (b << 1)
-		if i2c.SDA.Get() {
-			b |= 1
-		}
-		i2c.SCL.Low()
-		i2c.tick()
-	}
-	i2c.SDA.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	if ack {
-		i2c.SDA.Low()
-	} else {
-		i2c.SDA.High()
-	}
-	i2c.SCL.High()
-	i2c.tick()
-	i2c.SCL.Low()
-	i2c.SDA.High()
-	return b
-}
-
-// ---- I2C-compatible adapter for SSD1306 ----
-type softI2CWrapper struct {
-	bus *SoftI2C
-}
-
-func (s *softI2CWrapper) Tx(addr uint16, w, r []byte) error {
-	s.bus.start()
-	s.bus.writeByte(byte(addr<<1) | 0)
-	for _, b := range w {
-		s.bus.writeByte(b)
-	}
-	if r != nil {
-		s.bus.start()
-		s.bus.writeByte(byte(addr<<1) | 1)
-		for i := range r {
-			r[i] = s.bus.readByte(i < len(r)-1)
-		}
-	}
-	s.bus.stop()
-	return nil
-}
-
 // ---- MCP9808 ----
 const MCP9808_ADDR = 0x18
 
-func readTempC(i2c *softI2CWrapper) (float32, bool) {
-	buf := []byte{0x05}
-	i2c.Tx(MCP9808_ADDR, buf, nil)
-	data := make([]byte, 2)
-	i2c.Tx(MCP9808_ADDR, nil, data)
+func readTempC(bus drivers.I2C) (float32, bool) {
+	data := []byte{0, 0}
+	if err := bus.Tx(MCP9808_ADDR, []byte{0x05}, data); err != nil {
+		return 0, false
+	}
 
 	raw := uint16(data[0])<<8 | uint16(data[1])
 	temp := float32(raw&0x0FFF) * 0.0625
@@ -139,23 +38,22 @@ func blinkOnce(pin machine.Pin, duration time.Duration) {
 
 // ---- Main ----
 func main() {
-	i2c := SoftI2C{SDA: machine.D18, SCL: machine.D19}
-	i2c.Configure(100000)
-	bus := &softI2CWrapper{bus: &i2c}
+	i2c := i2csoft.New(machine.D19, machine.D18)
+	i2c.Configure(i2csoft.I2CConfig{Frequency: 100000, SCL: machine.D19, SDA: machine.D18})
 
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	display := ssd1306.NewI2C(bus)
-	display.Configure(ssd1306.Config{Width: 128, Height: 32})
+	display := ssd1306.NewI2C(i2c)
+	time.Sleep(100 * time.Millisecond) // let the OLED power rails settle before init
+	display.Configure(ssd1306.Config{Width: 128, Height: 32, Address: 0x3C})
 	display.ClearDisplay()
-	display.Display()
 
 	white := color.RGBA{255, 255, 255, 255}
 
 	for {
-		temp, ok := readTempC(bus)
-		display.ClearDisplay()
+		temp, ok := readTempC(i2c)
+		display.ClearBuffer()
 		if ok {
 			blinkOnce(led, 100*time.Millisecond)
 			tinyfont.WriteLine(display, &proggy.TinySZ8pt7b, 0, 16,
